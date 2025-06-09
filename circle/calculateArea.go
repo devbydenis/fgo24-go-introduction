ce8319edae8860a715c9b2e6d0ed918e9f683422
devbydenis <hello.denisrahmadi@gmail.com>
2025-06-09 17:46:04 +0700

    feat: seperate function into different file but in the same modules

---
+++ circle/calculateArea.go
@@ -0,0 +1,5 @@
+package circle
+
+func AreaCircle(r float32) float32 {
+	return r * r * 3.14159265358979323846
+}
---
+++ circle/calculatePerimeter.go
@@ -0,0 +1,5 @@
+package circle
+
+func CircumferenceCircle(r float32) float32 {
+	return 2 * r * 3.14159265358979323846
+}
